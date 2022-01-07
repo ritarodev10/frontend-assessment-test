96dd17f98f299b30c28417726f15eeb3694044c7
Geunta Buwono <geuntabuwonowork@gmail.com>
2022-01-07 09:53:05 +0700

    chore: add jest

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    [
+      'styled-components',
+      {
+        ssr: true,
+        displayName: true,
+        preprocess: false
+      }
+    ]
+  ],
+  env: {
+    production: {
+      plugins: ['babel-plugin-jsx-remove-data-test-id']
+    }
+  }
+};
